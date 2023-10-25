bc7f37a228f84bec64598533a0719c5a64209215
Marco-Sorrentino <94227887+marco-sorrentino@users.noreply.github.com>
2023-10-25 15:05:05 +0200

    done food card, get, animation, text propr

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_flutter_app/data/dummy_data.dart';
+import 'package:meals_flutter_app/screens/meals.dart';
 import 'package:meals_flutter_app/widgets/category_grid_item.dart';
+import 'package:meals_flutter_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(context) {
@@ -23,7 +39,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category));
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
